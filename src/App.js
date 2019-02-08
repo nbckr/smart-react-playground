2a63c1d639d9753ca5683ba07426d1265f68eafd
Niels Boecker <niels.boecker@gmail.com>
2019-02-08 19:08:24 +0000

    Use client js dependency instead of hard-coded import

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, { Component } from 'react'
-import { SmartSearch, ResourceList } from 'fhir-smartr' // FhirSmartr.SmartSearch and FhirSmartr.ResourceList in browser
+/* global FHIR */
+
+import React, {Component} from 'react'
+import {SmartSearch, ResourceList} from 'fhir-smartr' // FhirSmartr.SmartSearch and FhirSmartr.ResourceList in browser
 import PatientResource from './PatientResource';
 import AppointmentResource from './AppointmentResource';
+import "fhirclient/fhir-client";
 
 class App extends Component {
-  render() {
-    return (
-      // SmartSearch allows you to define a query
-      // The results of that query are then passed to its children as props.results
+    render() {
 
-      // Now just queries appointments for one patient (Shelby)
-      
-      <div>
+        FHIR.oauth2.ready(smart => {
+            smart.patient.read().then(result => console.log("Read result: ", result));
+            console.log("Patient id:" + smart.patient.id);
+            // now do something cool
+        });
 
-      <SmartSearch query={{ type: 'Appointment', query: { 'actor': 'Patient/d0d0cde0-4b21-42f6-9c1e-bfa447d72059' }}}>
-        <ResourceList>
-          <AppointmentResource />
-        </ResourceList>
-      </SmartSearch>
-      </div>
-    )
-  }
+
+        return (
+            // SmartSearch allows you to define a query
+            // The results of that query are then passed to its children as props.results
+
+            // Now just queries appointments for one patient (Shelby)
+
+            <div>
+                <SmartSearch
+                    query={{type: 'Appointment', query: {'actor': 'Patient/d0d0cde0-4b21-42f6-9c1e-bfa447d72059'}}}>
+                    <ResourceList>
+                        <AppointmentResource/>
+                    </ResourceList>
+                </SmartSearch>
+            </div>
+        )
+    }
 }
 
 export default App;
